7cc457534cefec8a7e398a065893c2da627a9b44
Sebastian Basiński <basinski.sebastian@gmail.com>
2019-05-14 21:29:40 +0200

    Add printMemory method

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-function Phone(brand, price, color) {
+function Phone(brand, model, price, color, memory) {
   this.brand = brand;
+  this.model = model;
   this.price = price;
   this.color = color;
+  this.memory = memory;
 }
 
 Phone.prototype.printInfo = function() {
-  console.log('The phone brand is ' + this.brand + ', color is ' + this.color + ' and the price is ' + this.price + '.');
+  console.log('The phone brand is ' + this.brand + this.model + ', color is ' + this.color + ' and the price is ' + this.price + '.');
 }
 
-var samsungGalaxyS6 = new Phone('Samsung', 1200, 'white');
-var iPhone6 = new Phone('Apple', 1800, 'silver');
-var onePlusOne = new Phone('One Plus', 800, 'blue');
+Phone.prototype.printMem = function() {
+	console.log(this.brand + ' ' + this.model + ' is equipped with ' + this.memory + ' of memory.');
+}
+
+var samsungGalaxyS6 = new Phone('Samsung', 'Galaxy S6', 1200, 'white', '124 GB');
+var iPhone6 = new Phone('Apple', 'iPhone 6s Plus', 1800, 'silver', '64 GB');
+var onePlusOne = new Phone('One Plus', 'One', 600, 'blue', '20 GB');
 
 samsungGalaxyS6.printInfo();
+samsungGalaxyS6.printMem();
+
 iPhone6.printInfo();
+iPhone6.printMem();
+
 onePlusOne.printInfo();
+onePlusOne.printMem();
